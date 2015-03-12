ba2dc5204cf008ca472794d2164f816a7cbd4706
Vitaly Puzrin <vitaly@rcdesign.ru>
2015-03-13 01:53:57 +0300

    Coverage fix

--- test/test.js
+++ test/test.js
@@ -36,16 +36,16 @@ describe('links', function () {
     if (next.trim()) {
       it('line ' + (idx + 1), function () {
         assert.ok(l.pretest(line), '(pretest failed in `' + line + '`)');
-        assert.ok(l.test(line), '(link not found in `' + line + '`)');
         assert.ok(l.test('\n' + line + '\n'), '(link not found in `\\n' + line + '\\n`)');
+        assert.ok(l.test(line), '(link not found in `' + line + '`)');
         assert.equal(l.match(line)[0].url, next);
       });
       skipNext = true;
     } else {
       it('line ' + (idx + 1), function () {
         assert.ok(l.pretest(line), '(pretest failed in `' + line + '`)');
-        assert.ok(l.test(line), '(link not found in `' + line + '`)');
         assert.ok(l.test('\n' + line + '\n'), '(link not found in `\\n' + line + '\\n`)');
+        assert.ok(l.test(line), '(link not found in `' + line + '`)');
         assert.equal(l.match(line)[0].url, line);
       });
     }
