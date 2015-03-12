71ab4a9c2cefee10e4ed29c0d8a5f615510ed8da
Vitaly Puzrin <vitaly@rcdesign.ru>
2015-03-12 22:02:25 +0300

    Generate demo sample directly from fixtures

--- support/demodata.js
+++ support/demodata.js
@@ -4,13 +4,57 @@
 
 /*eslint no-console:0*/
 
+'use strict';
+
 var fs   = require('fs');
 var path = require('path');
+
+function isComment(str) { return /^%.*/.test(str); }
+function isEmpty(str) { return !Boolean(str && str.trim()); }
+
+var result = [], lines, line, i;
+
+// Read links fixture
+lines = fs.readFileSync(path.join(__dirname, '../test/fixtures/links.txt'), 'utf8').split(/\r?\n/g);
+
+// Cleanup
+for (i = 0; i < lines.length; i++) {
+  line = lines[i];
+
+  if (isComment(line)) {
+    result.push(line);
+    continue;
+  }
+
+  if (isEmpty(line)) {
+    if (isComment(lines[i + 1])) {
+      result.push('');
+    }
+    continue;
+  }
+
+  result.push(line);
+
+  if (!isComment(lines[i + 1]) && !isEmpty(lines[i + 1])) {
+    i++;
+  }
+}
+
+result.push('');
+result.push('');
+
+// Read non-links fixture
+lines = fs.readFileSync(path.join(__dirname, '../test/fixtures/not_links.txt'), 'utf8').split(/\r?\n/g);
+
+result = result.concat(lines);
+
+// Join lines
+result = result.join('\n');
 
 console.log(JSON.stringify({
   self: {
     demo: {
-      source: fs.readFileSync(path.join(__dirname, './demo_template/sample.txt'), 'utf8')
+      source: result
     }
   }
 }, null, 2));
